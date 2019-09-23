3b6449b88f8eeda69134abc97b0de372fcd79da9
thomasmudge <alephcult@gmail.com>
2019-09-23 11:11:41 -0500

    Implemented ForagerBee.js class

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,16 +1,26 @@
-// [] Create a ForagerBee class, in pseudoclassical style, with:
-// [] call the Bee superclass
-// [] set the prototype
-// [] set the constructor
-// [] an age property that is set to 10
-// [] a job property that is set to find pollen
-// [] a color property inherited from bee that is set to yellow
-// [] a food property that is inherited from grub
-// [] an eat method that is inherited from grub
-// [] a canFly property that is set true
-// [] a treasureChest property that is set to an empty array[]
-// [] a forage method that allows the bee to add a treasure to the treasureChest
+// [X] Create a ForagerBee class, in pseudoclassical style, with:
+// [X] call the Bee superclass
+// [X] set the prototype
+// [X] set the constructor
+// [X] an age property that is set to 10
+// [X] a job property that is set to find pollen
+// [X] a color property inherited from bee that is set to yellow
+// [X] a food property that is inherited from grub
+// [X] an eat method that is inherited from grub
+// [X] a canFly property that is set true
+// [X] a treasureChest property that is set to an empty array[]
+// [X] a forage method that allows the bee to add a treasure to the treasureChest
 const ForagerBee = function() {
+    Bee.call(this);
+    this.age = 10;
+    this.job = 'find pollen';
+    this.color = 'yellow';
+    this.canFly = true;
+    this.treasureChest = [];
 };
-
+ForagerBee.prototype = Object.create(Bee.prototype);
+ForagerBee.prototype.constructor = ForagerBee;
+ForagerBee.prototype.forage = function(treasure){
+this.treasureChest.push(treasure);
+};
 window.ForagerBee = ForagerBee;
