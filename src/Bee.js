b331e0b35ffdb877784703fa86105fde4f82de17
thomasmudge <alephcult@gmail.com>
2019-09-23 10:53:10 -0500

    Setup js files with instructions as comments

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,12 @@
+// [ ] Create a Bee class, in pseudoclassical style, with:
+// [ ] call the Grub superclass
+// [ ] set the prototype
+// [ ] set the constructor
+// [ ] an age property that is set to 5
+// [ ] a color property that is set to yellow
+// [ ] a food property that is inherited from grub
+// [ ] an eat method that is inherited from grub
+// [ ] a job property that is set to keep on growing
 const Bee = function() {
 };
 
--- src/Grub.js
+++ src/Grub.js
@@ -1,4 +1,16 @@
+// [] Create a Grub class, in pseudoclassical style, with:
+// [] an age property that is set to 0
+// [] a color property that is set to pink
+// [] a food property that is set to jelly
+// [] an eat method
 const Grub = function() {
+    this.age = 0;
+    this.color = 'pink';
+    this.food = 'jelly';
+};
+
+Grub.prototype.eat = function(){
+//om nom nom nom
 };
 
 window.Grub = Grub;
